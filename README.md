a3dd0c6da77fa6d90b4d5c0f255743563b458c90
kimi <kimi.lai@tpisoftware.com>
2024-06-20 23:31:43 +0800

    Initialize gh-pages branch

---
+++ README.md
@@ -0,0 +1,10 @@
+# TravelIdea
+
+出外旅遊時，財務管理紀錄。
+
+build with :
+
+- Angular 14.2.0
+- bootstrap 5.2.0
+- FullCalendar plugin
+- Angular Material UI 13.0.0
---
+++ src/app/dialogs/dialogs.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Item } from '../table/table.component';
+
+@Component({
+  selector: 'app-dialogs',
+  templateUrl: './dialogs.component.html',
+  styleUrls: ['./dialogs.component.scss'],
+})
+export class DialogsComponent {
+  item: Item = {
+    name: '',
+    date: new Date(),
+    amount: 0,
+    selectedCurrency: 'TWD',
+    people: [],
+    shareAmount: 0,
+    editing: false,
+  };
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onSave(): void {
+    this.dialogRef.close(this.item);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+}
